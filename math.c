4aa77e2712ebd3b51bfe3d359f89fabe4f666c82
skech112 <127822548+skech112@users.noreply.github.com>
2023-10-12 22:20:22 +0900

    Update math.c

--- math.c
+++ math.c
@@ -1,13 +1,1 @@
-int add(int a, int b) {
-  return(a+b);
-}
 
-int subtract(int a, int b) {
-  return(a-b);
-}
-int multiply(int a, int b) {
-  return(a*b);
-}
-int divide(int a, int b) {
-  return(a/b);
-}
